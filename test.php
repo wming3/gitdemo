a71f6dde49fbdaa9e26ca9edaf07aa2f0a7caa86
wangtengtao <wangtengtao@iwaimai.baidu.com>
2016-12-12 16:51:01 +0800

    update

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Brief:
+ * Author: wangtengtao_iwm
+ * Date: 16/12/12
+ * Time: 下午4:48
+ */
+class Test{
+	public function run(){
+		echo 'wo cao', PHP_EOL;
+	}
+}
